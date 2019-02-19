69c4590b3d01074c5f7b472444d490cc656c29b6
Naresh Phadte <y>
2019-02-19 12:49:23 -0500

    Adding p-table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { TableModule} from 'primeng/table';
 import { HttpClientModule } from '@angular/common/http';
-import { TableModule} from 'primeng/table';
 
 import { AppComponent } from './app.component';
 import { ConatinerDataTableComponent } from './conatiner-data-table/conatiner-data-table.component';
--- src/app/services/container.service.ts
+++ src/app/services/container.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { ContainerEntry } from '../domain/containerentry';
+import {Car} from '../domain/car';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class ContainerService {
   constructor(private  http:HttpClient) { }
 
   getInitialContainers() {
-    return this.http.get('assets/showcase/data/initialcontainer_entry.json')
+    return this.http.get<any>('assets/showcase/data/initialcontainer_entries.json')
       .toPromise()
       .then(res => <ContainerEntry[]>res.data)
       .then(data => { return data; });
